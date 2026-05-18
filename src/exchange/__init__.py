f10c39eefd8d306d5668dc98ff07857b1c6c9db7
ajiteshm2003 <ajiteshm2003@gmail.com>
2026-05-18 18:45:56 -0400

    Add trade_log functions to module exports

--- src/exchange/__init__.py
+++ src/exchange/__init__.py
@@ -2,6 +2,7 @@ from .order import Order, OrderSide, OrderType, OrderStatus
 from .trade import Trade
 from .order_book import OrderBook
 from .matching_engine import MatchingEngine
+from .trade_log import execution_summary, trades_to_dataframe
 
 __all__ = [
     "Order",
@@ -11,4 +12,6 @@ __all__ = [
     "Trade",
     "OrderBook",
     "MatchingEngine",
+    "execution_summary",
+    "trades_to_dataframe",
 ]
