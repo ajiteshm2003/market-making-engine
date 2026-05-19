35f63b93a6369544dd49c42032fb18272b0fef68
ajiteshm2003 <ajiteshm2003@gmail.com>
2026-05-19 12:53:41 -0400

    Enhance performance module with new imports
    Added imports and updated __all__ for performance module.

--- src/performance/__init__.py
+++ src/performance/__init__.py
@@ -1,1 +1,16 @@
+"""src/performance/__init__.py"""
+from .profiler import (
+    profile_matching_engine, profile_simulation_loop, profile_memory,
+    run_full_benchmark, LatencyProfile, MemoryProfile, BenchmarkResult,
+)
+from .benchmarks import (
+    BenchmarkConfig, BENCHMARK_CONFIGS,
+    run_benchmark_suite, print_benchmark_table, print_cpp_path,
+)
 
+__all__ = [
+    "profile_matching_engine", "profile_simulation_loop", "profile_memory",
+    "run_full_benchmark", "LatencyProfile", "MemoryProfile", "BenchmarkResult",
+    "BenchmarkConfig", "BENCHMARK_CONFIGS",
+    "run_benchmark_suite", "print_benchmark_table", "print_cpp_path",
+]
