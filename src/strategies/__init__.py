40dcd4fe8f452e0a890898c929c8dbbe3873860c
ajiteshm2003 <ajiteshm2003@gmail.com>
2026-05-18 22:24:55 -0400

    Add AvellanedaStoikovMarketMaker to exports

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -7,6 +7,11 @@ from .base_market_maker import BaseMarketMaker
 from .inventory_aware_market_maker import InventoryAwareMarketMaker
 from .mm_metrics import MarketMakerMetrics, MMStepRecord
 from .naive_market_maker import NaiveMarketMaker
+from .avellaneda_stoikov_market_maker import (
+    AvellanedaStoikovMarketMaker,
+    ASConfig,
+    HorizonMode,
+)
 
 __all__ = [
     "BaseMarketMaker",
@@ -14,4 +19,7 @@ __all__ = [
     "MarketMakerMetrics",
     "MMStepRecord",
     "NaiveMarketMaker",
+    "AvellanedaStoikovMarketMaker",
+    "ASConfig",
+    "HorizonMode",
 ]
