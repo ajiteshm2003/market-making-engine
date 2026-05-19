2d98cc30872acd64a6d3dcd21a91e7446ee83a36
ajiteshm2003 <ajiteshm2003@gmail.com>
2026-05-19 11:10:51 -0400

    Add files via upload

---
+++ src/strategies/__init__-3.py
@@ -0,0 +1,31 @@
+"""
+src/strategies/__init__.py
+Public API for the strategies package.
+"""
+
+from .base_market_maker import BaseMarketMaker
+from .inventory_aware_market_maker import InventoryAwareMarketMaker
+from .mm_metrics import MarketMakerMetrics, MMStepRecord
+from .naive_market_maker import NaiveMarketMaker
+from .avellaneda_stoikov_market_maker import (
+    AvellanedaStoikovMarketMaker,
+    ASConfig,
+    HorizonMode,
+)
+from .regime_aware_as_market_maker import (
+    RegimeAwareAvellanedaStoikovMarketMaker,
+    RegimeAwareASConfig,
+)
+
+__all__ = [
+    "BaseMarketMaker",
+    "InventoryAwareMarketMaker",
+    "MarketMakerMetrics",
+    "MMStepRecord",
+    "NaiveMarketMaker",
+    "AvellanedaStoikovMarketMaker",
+    "ASConfig",
+    "HorizonMode",
+    "RegimeAwareAvellanedaStoikovMarketMaker",
+    "RegimeAwareASConfig",
+]
